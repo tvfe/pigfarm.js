2e607e0a59b5b8da26b78f1f29408fc9fc185054
僵尸浩 <zombieyang@tencent.com>
2016-09-16 21:18:05 +0800

    test coverage

--- index.js
+++ index.js
@@ -12,155 +12,155 @@ var createlog = nodedebug("auto-creating");
 var servelog = nodedebug("auto-serving");
 
 module.exports = function (config, options) {
-  options = options || {};
+	options = options || {};
 
-  assert.equal(typeof (config.data = config.data || {}), 'object', 'please give aotu.js a datasource map');
-  assert.equal(typeof config.render, 'function', 'please give aotu.js a render function');
+	assert.equal(typeof (config.data = config.data || {}), 'object', 'please give aotu.js a datasource map');
+	assert.equal(typeof config.render, 'function', 'please give aotu.js a render function');
 
-  // static data
-  var _staticJSON = {};
+	// static data
+	var _staticJSON = {};
 
-  createlog('reading data sources');
-  createHook(options.onServiceCreateStart, null)();
+	createlog('reading data sources');
+	createHook(options.onServiceCreateStart, null)();
 
-  // read data sources
-  var fetchers = {};
-  Object.keys(config.data).forEach(key=> {
-    var dataSource = config.data[key];
+	// read data sources
+	var fetchers = {};
+	Object.keys(config.data).forEach(key=> {
+		var dataSource = config.data[key];
 
-    if (dataSource.type == 'request') {
-      // request, fetch them when user's requests come in
-      // if this config is request, create fetchers
-      fetchers[key] = dataSource.action;
+		if (dataSource.type == 'request') {
+			// request, fetch them when user's requests come in
+			// if this config is request, create fetchers
+			fetchers[key] = dataSource.action;
 
-    } else if (dataSource.type == 'static') {
-      // static json, put it into result
-      var value = tryParseJson(dataSource.value);
-      _staticJSON[key] = value;
+		} else if (dataSource.type == 'static') {
+			// static json, put it into result
+			var value = tryParseJson(dataSource.value);
+			// _staticJSON[key] = value;
 
-      // put the static data into result
-      createInjector(key, _staticJSON)(value);
+			// put the static data into result
+			createInjector(key, _staticJSON)(value);
 
-    } else {
-      throw new Error('must indicate a type for datasource');
+		} else {
+			throw new Error('must indicate a type for datasource');
 
-    }
-  });
-  fetchers = fetchersFactory(fetchers);
-  createlog('readed data sources, static:', _staticJSON);
+		}
+	});
+	fetchers = fetchersFactory(fetchers);
+	createlog('readed data sources, static:', _staticJSON);
 
-    // var render = Renderer(config.template, config.helper);
-  var render = config.render;
+	// var render = Renderer(config.template, config.helper);
+	var render = config.render;
 
-  createHook(options.onServiceCreateEnd, null)();
+	createHook(options.onServiceCreateEnd, null)();
 
-  return function (fetchContext) {
-    var self = this;
+	return function (fetchContext) {
+		var self = this;
 
-    return co(function *() {
-      var hooks = {};
-      [
-        'onFetchStart',
-        'onFetchEnd',
-        'onRenderStart',
-        'onRenderEnd'
-      ].forEach(hookname=> {
-        hooks[hookname] = createHook(options[hookname], self);
-      });
+		return co(function *() {
+			var hooks = {};
+			[
+				'onFetchStart',
+				'onFetchEnd',
+				'onRenderStart',
+				'onRenderEnd'
+			].forEach(hookname=> {
+				hooks[hookname] = createHook(options[hookname], self);
+			});
 
-      servelog('start');
-      const contextParam = fetchContext || {};
+			servelog('start');
+			const contextParam = fetchContext || {};
 
-      // copy the staticJSON
-      var renderData = extend({}, contextParam, _staticJSON);
+			// copy the staticJSON
+			var renderData = extend({}, contextParam, _staticJSON);
 
-      var requestTree = {};
+			var requestTree = {};
 
-      servelog('fetch start');
-      hooks['onFetchStart']();
+			servelog('fetch start');
+			hooks['onFetchStart']();
 
-      // make the dependency tree for all requests
-      Object.keys(fetchers).forEach(key=> {
+			// make the dependency tree for all requests
+			Object.keys(fetchers).forEach(key=> {
 
-        requestTree[key] = {
-          dep: config.data[key].dependencies,
-          factory: datas=> {
+				requestTree[key] = {
+					dep: config.data[key].dependencies,
+					factory: datas=> {
 
-            return fetchers[key].call(this.autonodeContext, extend({}, datas, contextParam))
-              .then(function (ret) {
-                ret = ret.data;
+						return fetchers[key].call(this.autonodeContext, extend({}, datas, contextParam))
+							.then(function (ret) {
+								ret = ret.data;
 
-                return !ret && ret !== '' ? {} : ret
-              });
-          }
-        };
+								return !ret ? '' : ret
+							});
+					}
+				};
 
-      });
+			});
 
-      var fetched = yield runDependenciesTree.call(this, requestTree);
+			var fetched = yield runDependenciesTree.call(this, requestTree);
 
-      servelog('fetch end');
-      hooks['onFetchEnd']();
+			servelog('fetch end');
+			hooks['onFetchEnd']();
 
-      Object.keys(fetched).forEach(key=> {
-        let result = fetched[key];
-        let dep = config.data[key].dependencies;
-        if (dep && !config.data[key].mountatglobal) {
-          createInjector(key)(result, fetched[dep[0]])
+			Object.keys(fetched).forEach(key=> {
+				let result = fetched[key];
+				let dep = config.data[key].dependencies;
+				if (dep && !config.data[key].mountatglobal) {
+					createInjector(key)(result, fetched[dep[0]])
 
-        } else {
-          createInjector(key, renderData)(result);
+				} else {
+					createInjector(key, renderData)(result);
 
-        }
-      });
-      hooks['onRenderStart']();
-      // render
-      servelog('renderData keys', Object.keys(renderData));
+				}
+			});
+			hooks['onRenderStart']();
+			// render
+			servelog('renderData keys', Object.keys(renderData));
 
-      try {
-        var html = render(renderData);
+			try {
+				var html = render(renderData);
 
-      } catch (e) {
-        e.status = e.status || 555;
-        throw e;
-      }
-      hooks['onRenderEnd']();
+			} catch (e) {
+				e.status = e.status || 555;
+				throw e;
+			}
+			hooks['onRenderEnd']();
 
-      return html;
+			return html;
 
-    }).catch(e=> {
-      e.status = e.status || 503;
+		}).catch(e=> {
+			e.status = e.status || 503;
 
-      return Promise.reject(e);
-    })
-  }
+			return Promise.reject(e);
+		})
+	}
 };
 
 function createHook(fn, context) {
 
-  return function () {
-    try {
-      fn && fn.apply(context, arguments);
-    } catch (e) {
-    }
-  };
+	return function () {
+		try {
+			fn && fn.apply(context, arguments);
+		} catch (e) {
+		}
+	};
 }
 
 function noop() {
 }
 
 function tryParseJson(data) {
-  if (
-    typeof data == 'string' &&
-    (
-      (data[0] == "{" && data[data.length - 1] == "}") ||
-      (data[0] == "[" && data[data.length - 1] == "]")
-    )
-  ) {
-    try {
-      data = JSON.parse(data);
-    } catch (e) {
-    }
-  }
-  return data;
+	if (
+		typeof data == 'string' &&
+		(
+			(data[0] == "{" && data[data.length - 1] == "}") ||
+			(data[0] == "[" && data[data.length - 1] == "]")
+		)
+	) {
+		try {
+			data = JSON.parse(data);
+		} catch (e) {
+		}
+	}
+	return data;
 }
--- test/serve.test.js
+++ test/serve.test.js
@@ -7,125 +7,197 @@ var extend = require("extend");
 var templateCompiler = require("./es6templateCompiler");
 
 requestFactory.registerRequestor('default', function (cfg, callback) {
-  setTimeout(function () {
-    callback(null, {testdata: true});
-  }, 200);
+	setTimeout(function () {
+		callback(null, {testdata: true});
+	}, 200);
+});
+requestFactory.registerRequestor('error', function (cfg, callback) {
+	setTimeout(function () {
+		callback(new Error(cfg.url));
+	})
 });
 
 test('render error', async function () {
-  try {
-    await aotu({
-      render: templateCompiler('<html>\n' +
-        '<head></head>\n' +
-        '    <body>\n' +
-        '        <div>${undef.hehe}</div>\n' +
-        '    </body>\n' +
-        '</html>'),
-      data: {}
-    })();
-  } catch (e) {
-    assert.equal(e.status, 555);
-    assert(e.wrapper.indexOf('4 > |         <div>${undef.hehe}</div>') != -1);
-  }
+	try {
+		await aotu({
+			render: templateCompiler('<html>\n' +
+				'<head></head>\n' +
+				'    <body>\n' +
+				'        <div>${undef.hehe}</div>\n' +
+				'    </body>\n' +
+				'</html>'),
+			data: {}
+		})();
+	} catch (e) {
+		assert.equal(e.status, 555);
+		assert(e.wrapper.indexOf('4 > |         <div>${undef.hehe}</div>') != -1);
+	}
 });
 test('run helper', async function () {
-  var result = await aotu({
-    render: templateCompiler('${_.json(auto)}', {
-      json: function (data) {
-        return JSON.stringify(data);
-      }
-    }),
-    data: {
-      auto: {
-        type: "request",
-        action: {
-          url: "what://ever",
-          fixAfter: function (data) {
-            extend(data, {wocao: 1});
-            return data;
-          }
-        }
-      }
-    }
-  })();
-  assert.equal(result, '{"testdata":true,"wocao":1}');
+	var result = await aotu({
+		render: templateCompiler('${_.json(auto)}', {
+			json: function (data) {
+				return JSON.stringify(data);
+			}
+		}),
+		data: {
+			auto: {
+				type: "request",
+				action: {
+					url: "what://ever",
+					fixAfter: function (data) {
+						extend(data, {wocao: 1});
+						return data;
+					}
+				}
+			}
+		}
+	})();
+	assert.equal(result, '{"testdata":true,"wocao":1}');
 });
 test('requestEnd hook', async function () {
-  return new Promise(function (resolve, reject) {
-    aotu({
-      render: ()=> '<div></div>',
-      data: {
-        auto: {
-          type: "request",
-          action: {
-            url: "what://ever",
-            fixAfter: function (data) {
-              extend(data, {wocao: 1});
-              return data;
-            }
-          }
-        }
-      }
-    }, {
-      onFetchStart: function () {
-        this.autonodeContext = this.autonodeContext || {};
-        this.autonodeContext._timer = Date.now();
-      },
-      onRenderStart: function () {
-        throw new Error('hehe');
-      },
-      onRenderEnd: function () {
-        try {
-          assert(!!this.autonodeContext._timer, 'hook changing context fail');
-          // console.log(this.autonodeContext.timeline);
-          // for (var data of this.autonodeContext.timeline) {
-          // 	assert(Math.abs(data.duration.split('|')[1] / 1000 - 200) < 10);
-          // 	assert.equal(data.name, 'auto');
-          // }
-        } catch (e) {
-          return reject(e)
-        }
-        resolve()
-      }
-    }).call({});
-  })
+	return new Promise(function (resolve, reject) {
+		aotu({
+			render: ()=> '<div></div>',
+			data: {
+				auto: {
+					type: "request",
+					action: {
+						url: "what://ever",
+						fixAfter: function (data) {
+							extend(data, {wocao: 1});
+							return data;
+						}
+					}
+				}
+			}
+		}, {
+			onFetchStart: function () {
+				this.autonodeContext = this.autonodeContext || {};
+				this.autonodeContext._timer = Date.now();
+			},
+			onRenderStart: function () {
+				throw new Error('hehe');
+			},
+			onRenderEnd: function () {
+				try {
+					assert(!!this.autonodeContext._timer, 'hook changing context fail');
+					// console.log(this.autonodeContext.timeline);
+					// for (var data of this.autonodeContext.timeline) {
+					// 	assert(Math.abs(data.duration.split('|')[1] / 1000 - 200) < 10);
+					// 	assert.equal(data.name, 'auto');
+					// }
+				} catch (e) {
+					return reject(e)
+				}
+				resolve()
+			}
+		}).call({});
+	})
 });
 test('dependencies', async function () {
-  var result = await aotu({
-    render: templateCompiler('${JSON.stringify({auto, depglobal})}'),
-    data: {
-      auto: {
-        type: "request",
-        action: {
-          url: "what://ever",
-          fixAfter: function () {
-            var data = arguments[0];
-            extend(data, {wocao: 1});
-            return data;
-          }
-        }
-      },
-      depauto: {
-        type: "request",
-        action: {
-          url: "what://ever",
-          fixAfter: function () {
-            var data = arguments[0];
-            extend(data, {dep: true});
-            return data;
-          }
-        },
-        dependencies: ['auto']
-      },
-      depglobal: {
-        type: "request",
-        action: {
-          url: "what://ever"
-        },
-        dependencies: ['auto'],
-        mountatglobal: true
-      }
-    }
-  })();
-  assert.equal(result, '{"auto":{"testdata":true,"wocao":1,"depauto":{"testdata":true,"dep":true}},"depglobal":{"testdata":true}}');
+	var result = await aotu({
+		render: function (data) {
+			return JSON.stringify({
+				auto: data.auto,
+				depglobal: data.depglobal
+			})
+		},
+		data: {
+			auto: {
+				type: "request",
+				action: {
+					url: "what://ever",
+					fixAfter: function () {
+						var data = arguments[0];
+						extend(data, {wocao: 1});
+						return data;
+					}
+				}
+			},
+			depauto: {
+				type: "request",
+				action: {
+					url: "what://ever",
+					fixAfter: function () {
+						var data = arguments[0];
+						extend(data, {dep: true});
+						return data;
+					}
+				},
+				dependencies: ['auto']
+			},
+			depglobal: {
+				type: "request",
+				action: {
+					url: "what://ever"
+				},
+				dependencies: ['auto'],
+				mountatglobal: true
+			}
+		}
+	})();
+	assert.equal(result, '{"auto":{"testdata":true,"wocao":1,"depauto":{"testdata":true,"dep":true}},"depglobal":{"testdata":true}}');
+});
+test('requestError', async function () {
+	try {
+		await aotu({
+			data: {
+				err: {
+					type: "request",
+					action: {
+						url: "error://hehe",
+						onError: e=> {
+							e.status = 302;
+							return e;
+						}
+					}
+				}
+			},
+			render: function (data) {
+				return JSON.stringify(data);
+			}
+		})();
+	} catch (e) {
+		assert.equal(e.status, 302);
+	}
+});
+test('static data', async function () {
+	var result = await aotu({
+		data: {
+			'ret.json': {
+				type: "static",
+				value: '{"hehe": 1}'
+			},
+			'ret.request': {
+				type: "request",
+				action: {
+					url: "what://ever"
+				}
+			}
+		},
+		render: function (data) {
+			return data;
+		}
+	})();
+	assert.equal(result.ret.json.hehe, 1);
+	assert(result.ret.request.testdata);
+	assert.equal(result['ret.json'], void 0);
+});
+test('invalid data source', async function () {
+	try {
+		aotu({
+			data: {
+				sth: {
+					value: "{}"
+				}
+			},
+			render: function () {
+				return 'abc'
+			}
+		})
+	} catch(e) {
+		assert.equal(e.message, 'must indicate a type for datasource');
+	}
+
 });
